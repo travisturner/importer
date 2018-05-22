32274e49fd45fead91021cd4f86093d183d98233
Travis <travis@pilosa.com>
2018-05-22 14:52:47 -0500

    add a single BSI frame

--- consumer/main.go
+++ consumer/main.go
@@ -38,8 +38,8 @@ func (s *EventSource) Record() (interface{}, error) {
 	}
 	for _, f := range s.Frames {
 		pr.AddRow(f, uint64(rand.Intn(100)))
-		//pr.AddRow(f, 1)
 	}
+	pr.AddVal("bb", "fld", int64(rand.Intn(100000)))
 	s.count++
 
 	if s.count%100000 == 0 {
@@ -49,58 +49,31 @@ func (s *EventSource) Record() (interface{}, error) {
 	return pr, nil
 }
 
-/*
-type PilosaRecord struct {
-    Col  uint64
-    Rows []Row
-    Vals []Val
-}
-
-// AddVal adds a new value to be range encoded into the given field to the
-// PilosaRecord.
-func (pr *PilosaRecord) AddVal(frame, field string, value int64) {
-    pr.Vals = append(pr.Vals, Val{Frame: frame, Field: field, Value: value})
-}
-
-// AddRow adds a new bit to be set to the PilosaRecord.
-func (pr *PilosaRecord) AddRow(frame string, id uint64) {
-    pr.Rows = append(pr.Rows, Row{Frame: frame, ID: id})
-}
-
-// AddRowTime adds a new bit to be set with a timestamp to the PilosaRecord.
-func (pr *PilosaRecord) AddRowTime(frame string, id uint64, ts time.Time) {
-    pr.Rows = append(pr.Rows, Row{Frame: frame, ID: id, Time: ts})
-}
-
-// Row represents a bit to set in Pilosa sans column id (which is held by the
-// PilosaRecord containg the Row).
-type Row struct {
-    Frame string
-    ID    uint64
-
-    // Time is the timestamp for the bit in Pilosa which is the intersection of
-    // this row and the Column in the PilosaRecord which holds this row.
-    Time time.Time
-}
-
-// Val represents a BSI value to set in a Pilosa field sans column id (which is
-// held by the PilosaRecord containing the Val).
-type Val struct {
-    Frame string
-    Field string
-    Value int64
-}
-
-*/
-
-// Run begins indexing data from Kafka into Pilosa.
+// Run begins indexing data into Pilosa.
 func (m *Main) Run() error {
 	src := &EventSource{
 		Frames: []string{"fa", "fb", "fc", "fd", "fe", "ff", "fg", "fh", "fi", "fj"},
-		//Frames: []string{"fa"},
 	}
 
-	indexer, err := pdk.SetupPilosa(m.PilosaHosts, m.Index, []pdk.FrameSpec{}, m.BatchSize)
+	fields := []pdk.FieldSpec{
+		pdk.FieldSpec{
+			Name: "fld",
+			Min:  0,
+			Max:  100000,
+		},
+	}
+
+	frames := []pdk.FrameSpec{
+		pdk.FrameSpec{
+			Name:           "bb",
+			CacheSize:      0,
+			InverseEnabled: false,
+			Fields:         fields,
+		},
+	}
+
+	//indexer, err := pdk.SetupPilosa(m.PilosaHosts, m.Index, []pdk.FrameSpec{}, m.BatchSize)
+	indexer, err := pdk.SetupPilosa(m.PilosaHosts, m.Index, frames, m.BatchSize)
 	if err != nil {
 		return errors.Wrap(err, "setting up Pilosa")
 	}
